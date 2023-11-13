e83ef2cb03241838fb829d5b09bdb80c3a3e8f69
kimhamney <kimhamney@gmail.com>
2023-11-13 20:13:43 +0900

    edit tweet text

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -1,9 +1,10 @@
 import { auth, db, storage } from "../firebase";
-import { deleteDoc, doc } from "firebase/firestore";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
 import { deleteObject, ref } from "firebase/storage";
 
 import { ITweet } from "./timeline";
 import styled from "styled-components";
+import { useState } from "react";
 
 const Wrapper = styled.div`
   display: grid;
@@ -31,6 +32,27 @@ const Payload = styled.p`
   font-size: 18px;
 `;
 
+const EditTextArea = styled.textarea`
+  border: 2px solid white;
+  margin: 10px 0px 10px;
+  padding: 20px;
+  border-radius: 20px;
+  font-weight: 16px;
+  color: white;
+  background-color: black;
+  width: 100%;
+  resize: none;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  &::placeholder {
+    font-size: 16px;
+  }
+  &:focus {
+    outline: none;
+    border-color: #1d9bf0;
+  }
+`;
+
 const DeleteButton = styled.button`
   background-color: tomato;
   color: white;
@@ -43,8 +65,42 @@ const DeleteButton = styled.button`
   cursor: pointer;
 `;
 
+const EditTextButton = styled.button`
+  background-color: #1d9bf0;
+  color: white;
+  font-weight: 600;
+  border: 0;
+  font-size: 12px;
+  padding: 5px 10px;
+  text-transform: uppercase;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
 export default function Tweet({ username, photo, tweet, userId, id }: ITweet) {
   const user = auth.currentUser;
+  const [isEdit, setEdit] = useState(false);
+  const [text, setText] = useState("");
+  // edit photo
+  const onTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.target.value);
+  };
+  const onEditClick = async () => {
+    if (user?.uid !== userId) return;
+    setEdit(true);
+    try {
+      if (isEdit) {
+        await updateDoc(doc(db, "tweets", id), {
+          tweet: text,
+        });
+        setEdit(false);
+      }
+    } catch (e) {
+      console.log(e);
+      setEdit(false);
+    } finally {
+    }
+  };
   const onDelete = async () => {
     const ok = confirm("Are you sure you want to delete this tweet?");
     if (!ok || user?.uid !== userId) return;
@@ -63,9 +119,22 @@ export default function Tweet({ username, photo, tweet, userId, id }: ITweet) {
     <Wrapper>
       <Column>
         <Username>{username}</Username>
-        <Payload>{tweet}</Payload>
+        {isEdit ? (
+          <EditTextArea
+            required
+            rows={5}
+            maxLength={180}
+            onChange={onTextChange}
+            placeholder={tweet}
+          />
+        ) : (
+          <Payload>{tweet}</Payload>
+        )}
         {user?.uid === userId ? (
-          <DeleteButton onClick={onDelete}>Delete</DeleteButton>
+          <>
+            <EditTextButton onClick={onEditClick}>Edit</EditTextButton>
+            <DeleteButton onClick={onDelete}>Delete</DeleteButton>
+          </>
         ) : null}
       </Column>
       <Column>{photo ? <Photo src={photo} /> : null}</Column>
